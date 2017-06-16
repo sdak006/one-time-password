889c35ac6abf6fcc618a67a74d7f26a861df4f6f
Jade\Scott <sdak006@gmail.com>
2017-06-16 15:21:51 -0500

    first commit

---
+++ functions/index.js
@@ -0,0 +1,5 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const createUser = require('./create_user');
+
+exports.createUser = functions.https.onRequest(createUser);
